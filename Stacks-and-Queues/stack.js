234a40962008ee955c2baa16ae92d3e603b578dd
Tanner Richards <Tanner.richards990@gmail.com>
2023-01-26 17:53:42 -0800

    added files for managing challenges

---
+++ Stacks-and-Queues/stack.js
@@ -0,0 +1,29 @@
+class Stack {
+  constructor() {
+    this.array = [];
+  }
+
+  push(item) {
+    this.array.push(item);
+  }
+
+  pop() {
+    if (this.array.length === 0) {
+      return null;
+    }
+    return this.array[this.array.length - 1];
+  }
+}
+
+const stack = new Stack();
+stack.push("fox");
+stack.push("goose");
+stack.push("lizard");
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+stack.push("llama");
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.pop());
