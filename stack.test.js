9ef73f020c536fe7885422f1dc6ddd52b7a12c78
Tanner Richards <Tanner.richards990@gmail.com>
2023-01-29 17:23:37 -0800

    removed all skips on test

--- stack.test.js
+++ stack.test.js
@@ -7,31 +7,31 @@ describe("Stack", () => {
     stack = new Stack();
   });
 
-  test.skip("push() adds an item to the stack", () => {
+  test("push() adds an item to the stack", () => {
     stack.push("fox");
     stack.push("goose");
     expect(stack.array).toEqual(["fox", "goose"]);
   });
 
-  test.skip("pop() removes and returns the last item in the stack", () => {
+  test("pop() removes and returns the last item in the stack", () => {
     stack.push("fox");
     stack.push("goose");
     expect(stack.pop()).toBe("goose");
     expect(stack.array).toEqual(["fox"]);
   });
 
-  test.skip("pop() returns null when the stack is empty", () => {
+  test("pop() returns null when the stack is empty", () => {
     expect(stack.pop()).toBeNull();
   });
 
-  test.skip("peek() returns the last item in the stack", () => {
+  test("peek() returns the last item in the stack", () => {
     stack.push("fox");
     stack.push("goose");
     expect(stack.peek()).toBe("goose");
     expect(stack.array).toEqual(["fox", "goose"]);
   });
 
-  test.skip("peek() returns null when the stack is empty", () => {
+  test("peek() returns null when the stack is empty", () => {
     expect(stack.peek()).toBeNull();
   });
 });
